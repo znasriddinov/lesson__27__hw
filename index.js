8c89eced88ab29cd9fce0072217d7e6d3332a542
znasriddinov <143064598+znasriddinov@users.noreply.github.com>
2023-10-11 13:41:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// for 1
+
+/*
+let k = parseInt(prompt("k ni kiriting"));
+let n = parseInt(prompt("n ni kiriting"));
+
+for (let i = 0; i < n; i++) {
+  alert(k);
+}
+*/
+
+// for 2
+
+/*
+let a = parseInt(prompt("a ni kiriting"));
+let b = parseInt(prompt("b ni kiriting"));
+
+for (let i = a; i <= b; i++) {
+  alert(i);
+}
+*/
+
+// for 3
+
+/*
+let a = parseInt(prompt("a ni kiriting"));
+let b = parseInt(prompt("b ni kiriting"));
+
+for (let i = b; i > a; i--) {
+  alert(i);
+}
+*/
+
+// for 4
+
+/*
+let cost = parseInt(prompt("1 kg konfetni narxini kiriting"));
+
+for (let i = 1; i <= 10; i++) {
+  alert(i * cost);
+}
+*/
+
+// for 6
+
+let cost = parseInt(prompt("1 kg konfetni narxini kiriting"));
+
+for (let i = 1; i < 2; i++) {
+  
+}
